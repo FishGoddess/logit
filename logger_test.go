1709d2d534a7b0d5e76d8088cf5d8ac763d7a1ff
FishGoddess <1149062639@qq.com>
2020-03-27 15:47:04 +0800

    v0.1.0 基本完成，等待做并发测试验证

--- logger_test.go
+++ logger_test.go
@@ -19,8 +19,10 @@
 package logit
 
 import (
+    "fmt"
     "os"
     "strconv"
+    "strings"
     "sync"
     "testing"
 )
@@ -62,16 +64,16 @@ func TestLoggerEnable(t *testing.T) {
 // 测试日志记录器的级别控制是否可用
 func TestLevel(t *testing.T) {
     logger := NewLogger(DebugLevel, NewDefaultHandler(os.Stdout, NewDefaultEncoder(DefaultTimeFormat)))
-    logger.Warn(logger.Level().String())
+    logger.Debug(logger.Level().String())
     logger.Info("这条 info 级别的内容可以显示吗？")
-    logger.Error("这条 error 级别的内容可以显示吗？")
     logger.Warn("这条 warn 级别的内容可以显示吗？")
+    logger.Error("这条 error 级别的内容可以显示吗？")
 }
 
 // 测试更改日志级别是否可用
 func TestLoggerChangeLevelTo(t *testing.T) {
-    logger := NewLogger(DebugLevel, NewDefaultHandler(os.Stdout, NewDefaultEncoder(DefaultTimeFormat)))
-    logger.Info("Log level is warn, so info message will not be logged!")
+    logger := NewLogger(WarnLevel, NewDefaultHandler(os.Stdout, NewDefaultEncoder(DefaultTimeFormat)))
+    logger.Info("Logger's level is warn, so info message will not be logged!")
 
     logger.ChangeLevelTo(InfoLevel)
     logger.Info("Now info message will be logged!")
@@ -90,59 +92,67 @@ func TestLoggerEnableAndDisableFileInfo(t *testing.T) {
     logger.Warn("现在应该没有文件信息了吧！")
 }
 
+type myHandler struct{}
+
+// Customize your own handler.
+func (mh *myHandler) Handle(log *Log) bool {
+    os.Stdout.WriteString("handler1: " + log.Msg() + "\n")
+    return true
+}
+
 // 测试增加处理器是否可用
-//func TestLoggerAddHandlersAndSetHandlers(t *testing.T) {
-//    logger := NewLogger(os.Stdout, InfoLevel)
-//    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
-//
-//    handlers1 := func(logger *Logger, level Level, now time.Time, msg string) bool {
-//        logger.writer.Write([]byte("第一个日志处理器！\n"))
-//        return true
-//    }
-//
-//    handlers2 := func(logger *Logger, level Level, now time.Time, msg string) bool {
-//        logger.writer.Write([]byte("第二个日志处理器！\n"))
-//        return true
-//    }
-//
-//    logger.AddHandlers(handlers1, handlers2)
-//    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
-//
-//    ok := logger.SetHandlers()
-//    if ok {
-//        t.Fatal("SetHandlers 应该返回 false！")
-//    }
-//    logger.SetHandlers(handlers1, handlers2)
-//    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
-//}
+func TestLoggerAddHandlersAndSetHandlers(t *testing.T) {
+    logger := NewLogger(DebugLevel, NewDefaultHandler(os.Stdout, NewDefaultEncoder(DefaultTimeFormat)))
+    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
+
+    if len(logger.handlers) != 1 {
+        t.Fatal("处理器个数不正确！")
+    }
+
+    logger.AddHandlers(&myHandler{})
+    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
+
+    if len(logger.handlers) != 2 {
+        t.Fatal("处理器个数不正确！")
+    }
+
+    ok := logger.SetHandlers()
+    if ok {
+        t.Fatal("SetHandlers 应该返回 false！")
+    }
+
+    logger.SetHandlers(&myHandler{})
+    logger.Info("当前的日志处理器：" + fmt.Sprintf("%v", logger.handlers))
+
+    if len(logger.handlers) != 1 {
+        t.Fatal("处理器个数不正确！")
+    }
+}
 
 // 测试获取日志处理器的方法
-//func TestLoggerHandlers(t *testing.T) {
-//    logger := NewStdoutLogger(InfoLevel)
-//    handlers := logger.Handlers()
-//
-//    // 需要判断地址是否一样，一样说明有问题
-//    if &handlers[0] == &logger.handlers[0] {
-//        t.Fatal("handlers 获取的数据和底层数据地址一样！这是有问题的！")
-//    }
-//
-//    // 显示每个日志处理器的地址
-//    logger.Info("handlers 个数：" + strconv.Itoa(len(handlers)))
-//    logger.InfoFunc(func() string {
-//        builder := strings.Builder{}
-//        for _, handler := range handlers {
-//            builder.WriteString(fmt.Sprintf("%p ", &handler))
-//        }
-//        return builder.String()
-//    })
-//
-//    // 尝试非法篡改日志处理器
-//    handlers[0] = func(logger *Logger, level Level, now time.Time, msg string) bool {
-//        fmt.Println("哈哈哈被非法篡改了！！！")
-//        return false
-//    }
-//    logger.Warn("这条信息需要显示出来！")
-//}
+func TestLoggerHandlers(t *testing.T) {
+    logger := NewDevelopLogger()
+    handlers := logger.Handlers()
+
+    // 需要判断地址是否一样，一样说明有问题
+    if &handlers[0] == &logger.handlers[0] {
+        t.Fatal("handlers 获取的数据和底层数据地址一样！这是有问题的！")
+    }
+
+    // 显示每个日志处理器的地址
+    logger.Info("handlers 个数：" + strconv.Itoa(len(handlers)))
+    logger.InfoFunc(func() string {
+        builder := strings.Builder{}
+        for _, handler := range handlers {
+            builder.WriteString(fmt.Sprintf("%p ", &handler))
+        }
+        return builder.String()
+    })
+
+    // 尝试非法篡改日志处理器
+    handlers[0] = &myHandler{}
+    logger.Warn("这条信息需要显示出来！")
+}
 
 // 测试并发情况下使用 Logger
 func TestLoggerInConcurrency(t *testing.T) {
