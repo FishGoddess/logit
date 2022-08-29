a5c6a9f8b99ca53a497823e65ae619468e75de59
FishGoddess <1149062639@qq.com>
2022-08-29 11:41:56 +0800

    增加测试

--- extension/config/config_test.go
+++ extension/config/config_test.go
@@ -14,9 +14,85 @@
 
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/go-logit/logit"
+	"github.com/go-logit/logit/support/global"
+)
 
 // go test -v -cover -run=^TestConfigOptions$
 func TestConfigOptions(t *testing.T) {
+	cfg := Config{
+		Level:         LevelDebug,
+		TimeKey:       "log.time",
+		LevelKey:      "log.level",
+		MsgKey:        "log.msg",
+		PIDKey:        "log.pid",
+		FileKey:       "log.file",
+		LineKey:       "log.line",
+		FuncKey:       "log.func",
+		TimeFormat:    UnixTimeFormat,
+		WithPID:       true,
+		WithCaller:    true,
+		CallerDepth:   global.CallerDepth,
+		AutoFlush:     "30s",
+		Appender:      AppenderText,
+		DebugAppender: AppenderText,
+		InfoAppender:  AppenderText,
+		WarnAppender:  AppenderText,
+		ErrorAppender: AppenderText,
+		PrintAppender: AppenderText,
+		Writer: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+		DebugWriter: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+		InfoWriter: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+		WarnWriter: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+		ErrorWriter: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+		PrintWriter: WriterConfig{
+			Target:     WriterTargetStdout,
+			Mode:       WriterModeDirect,
+			FileName:   "",
+			BufferSize: "4MB",
+			BatchCount: 1024,
+		},
+	}
 
+	opts, err := cfg.Options()
+	if err != nil {
+		t.Error(err)
+	}
+
+	logger := logit.NewLogger(opts...)
+	defer logger.Close()
+	logger.Info("My mother is a config").Any("config", cfg).Log()
 }
